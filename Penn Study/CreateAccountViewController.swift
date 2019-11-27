1c6030e87dc585968ba49f8d60854dc10010890a
Brandon Barros <brandonbarros12@gmail.com>
2019-11-26 20:03:14 -0500

    Firebase database added

---
+++ Penn Study/CreateAccountViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateAccountViewController.swift
+//  
+//
+//  Created by Brandon Barros on 11/26/19.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class CreateAccountViewController: UIViewController {
+
+    
+    let ref = Database.database().reference()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
